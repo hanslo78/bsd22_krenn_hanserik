62793e4e25b319e7cba38e4688e2c502dbc80a7a
hanslo78 <krennhanserik788@gmail.com>
2023-05-11 18:16:16 +0200

    created exercise3.md and updatedt .gitignore

---
+++ exercise3.md
@@ -0,0 +1,20 @@
+# Exercise 3
+
+## What i did
+ 
+- Install maven
+- Install JDK
+- ???
+- Created the Maven Project in this one
+- Added the target folder 
+- Written some smart methods for my Calculator Programm
+
+
+### Images
+
+Folder structure
+![](resources/images/ex3_1.jpg)
+
+Build
+![](resources/images/ex3_2.jpg)
+
---
+++ src/main/java/at/campus02/bsd/Calculator.java
@@ -0,0 +1,19 @@
+public class Calculator {
+
+    public double add(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    public double minus(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    public double divide(double num1, double num2) {
+        return num1 / num2;
+    }
+
+    public double multiply(double num1, double num2) {
+        return num1 * num2;
+    }
+
+}
